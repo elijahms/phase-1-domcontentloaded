a5241d98103dc6df32213e2f0a21bd59076d0144
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-18 10:54:26 -0400

    commit tree a0b75d6898cf1f06001fbe7e95a1afc6f604290f

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const changeIt = document.getElementById("text");
+    changeIt.textContent = "This is really cool!"
+  });
+  
